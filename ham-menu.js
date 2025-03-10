a50ddca94c747325ae6111f3eb1eb0f12f756c3f
DavidOng122 <jingxiangojx@gmail.com>
2025-03-10 10:50:26 -0500

    changed

--- ham-menu.js
+++ ham-menu.js
@@ -3,7 +3,6 @@ const offScreenMenu = document.querySelector('.off-screen-menu');
 const hamburger = document.querySelector('.hamburger-menu');
 
 function toggleMenu() {
-    
     offScreenMenu.classList.toggle('active'); 
     hamburger.classList.toggle('active'); // Toggle X animation
 }
